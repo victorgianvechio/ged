f4f14222e7d35f7053d7ab6cb0bba7cf78cdf17a
Victor Gianvechio <victor.gianvecchio@live.com>
2022-08-30 20:03:22 -0300

    ajustado arquivos de configuração

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import './lib/dotenv';
+import './config/dotenv';
 import App from './App';
 import { createPublicDir } from './utils/utils';
 
@@ -7,5 +7,7 @@ const port = process.env.PORT || process.env.APP_PORT;
 createPublicDir();
 
 App.listen(port, () => {
-  console.log(`[${process.env.NODE_ENV}] GED API is running on port ${port}`);
+  console.log(
+    `[${process.env.NODE_ENV}] ${process.env.APP_NAME} is running on port ${port}`
+  );
 });

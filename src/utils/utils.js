8684466ac3ebf6a0ce2503286f0eea348c969dcb
Victor Gianvechio <victor.gianvecchio@live.com>
2022-09-01 17:31:06 -0300

    adicionado rootPath

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,6 +3,8 @@ import fs from 'fs';
 
 export const publicPath = resolve(__dirname, '..', '..', 'public');
 
+export const rootPath = resolve(__dirname, '..', '..');
+
 export function createPublicDir() {
   if (!fs.existsSync(publicPath)) {
     fs.mkdirSync(publicPath);

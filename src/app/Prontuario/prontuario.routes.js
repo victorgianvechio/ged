04d3369e17ae8b9323cddda2a33262b617eb44c5
Victor Gianvechio <victor.gianvecchio@live.com>
2022-09-27 20:26:13 -0300

    ajustado controller

--- src/app/Prontuario/prontuario.routes.js
+++ src/app/Prontuario/prontuario.routes.js
@@ -1,14 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import ProntuarioController from './ProntuarioController';
+import multerConfig from '../../config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
-routes.get('/', ProntuarioController.getAll);
-routes.get('/:id', ProntuarioController.getById);
-routes.post('/,', ProntuarioController.create);
-routes.put('/:id', ProntuarioController.update);
-routes.delete('/:id', ProntuarioController.delete);
-routes.delete('/:id/email', ProntuarioController.delete);
+routes.get('/', ProntuarioController.index);
+routes.get('/:id', ProntuarioController.getByProntuario);
+routes.get('/advanced-search', ProntuarioController.advancedSearch);
+routes.post('/,', ProntuarioController.storeTest);
+routes.post(
+  '/prontuario-upload,',
+  upload.single('file'),
+  ProntuarioController.store
+);
+
+// routes.put('/:id', ProntuarioController.update);
+// routes.delete('/:id', ProntuarioController.delete);
+// routes.delete('/:id/email', ProntuarioController.delete);
 
 export default routes;
